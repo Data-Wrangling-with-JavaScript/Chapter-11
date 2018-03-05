c2b394e97976d2bc4da55d19ea2e095537ff8ae0
Ashley Davis <ashley@codecapers.com.au>
2018-03-06 09:37:53 +1000

    Listing 1. A template chart.

---
+++ listing-1/app.js
@@ -0,0 +1,53 @@
+//
+// Simplest possible example of a C3 chart in a web page.
+//
+// Based on the example line chart: http://c3js.org/samples/simple_multiple.html
+//
+// To run this, first install bower, then install live-server:
+//
+//      npm install -g bower
+//
+//      npm install -g live-server
+//
+// Then change directory to the same directory as this file and install dependencies:
+//
+//      bower install
+//
+// Then run live server from the same directory as this file:
+//
+//      live-server
+//
+// Your browser will open and a line chart will be rendered.
+//
+
+"use strict";
+
+$(function () {
+
+    $.get("nyc-temperature.csv")
+        .then(function (response) {
+            var parseOptions = {
+                header: true,
+                dynamicTyping: true
+            };
+            var parsed = Papa.parse(response, parseOptions);
+
+            var chart = c3.generate({
+                bindto: "#chart",
+                data: {
+                    json: parsed.data,
+                    keys: {
+                        x: "Year", // Specify the CSV file column to use as the X axis.
+                        value: [
+                            "AvgTemp"
+                        ]
+                    }
+                }
+            });
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
+});
+
