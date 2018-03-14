b01bbca60f7ec8f3559a167c762ceff3c2c17afa
Ashley Davis <ashley@codecapers.com.au>
2018-03-15 09:35:55 +1000

    Refactored out reusable functions.

--- listing-5/index.js
+++ listing-5/index.js
@@ -3,16 +3,25 @@
 const webServer = require('./web-server.js');
 const Nightmare = require('nightmare');
 
+//
+// Capture the web page specified by URL to the specifed image file.
+//
+function captureWebPage (urlToCapture, captureElementSelector, outputImagePath) {
+    console.log("<< " + urlToCapture);
+    console.log(">> " + outputImagePath);
+
+    const nightmare = Nightmare(); // Create an Nightmare instance.
+    return nightmare.goto(urlToCapture) // Point the browser at the requested web page.
+        .wait(captureElementSelector) // Wait until the specified HTML element appears on the screen. 
+        .screenshot(outputImagePath) // Capture a screenshot to an image file.
+        .end(); // End the Nightmare session. Any queued operations are complated and the headless browser is terminated.
+};
+
 webServer.start()
     .then(server => {
+        const urlToCapture = "http://localhost:3000";
         const outputImagePath = "./output/nyc-temperatures.png";
-        console.log(">> " + outputImagePath);
-
-        const nightmare = Nightmare(); // Create an Nightmare instance.
-        return nightmare.goto("http://localhost:3000") // Point the browser at the web server we just started.
-            .wait("svg") // Wait until the graphics for the chart appear on screen. 
-            .screenshot(outputImagePath) // Capture a screenshot to an image file.
-            .end() // End the Nightmare session. Any queued operations are complated and the headless browser is terminated.
+        return captureWebPage(urlToCapture, "svg", outputImagePath)
             .then(() => server.close()); // Stop the web server when we are done.
     })
     .then(() => {
@@ -21,4 +30,4 @@ webServer.start()
     .catch(err => {
         console.error("Something went wrong :(");
         console.error(err);
-    })
+    });
--- listing-7/index.js
+++ listing-7/index.js
@@ -3,35 +3,46 @@
 const webServer = require('./web-server.js');
 const Nightmare = require('nightmare');
 
+//
+// Capture a multi-page report to a PDF file.
+//
+function captureReport (urlToCapture, captureElementSelector, outputImagePath) {
+    console.log("<< " + urlToCapture);
+    console.log(">> " + outputImagePath);
+
+    const nightmare = Nightmare(); // Create an Nightmare instance.
+    return nightmare.goto(urlToCapture) // Point the browser at the requested web page.
+        .wait(captureElementSelector) // Wait until the specified HTML element appears on the screen. 
+        .evaluate(() => { // Evaluate JavaScript code within the headless browser. This function returns a promise which changes the way our code works.
+            const body = document.querySelector("body"); // Find the body element of the web page.
+            return { // Return details computed in the headless browser to Node.js.
+                documentArea: { // Return the scrollable area of the page, we will expand the size of the browser window to cover the entire documents (thus removing any scrollbars we might otherwise capture).
+                    width: body.scrollWidth,
+                    height: body.scrollHeight
+                },
+            };
+        })
+        .then(pageDetails => { // Retrieve details computed in the headless browser. We can now use these value in subsequent Node.js code.
+            const outputImagePath = "./output/nyc-temperature-report.pdf";
+            console.log(">> " + outputImagePath);
+            const printOptions = {
+                marginsType: 0, // No margins. We want to be able to set our margins explicitly in CSS.
+                pageSize: { // The size of each page. These values match the specification for the A4 page size standard, but in landscape.
+                    width: 297000, // 29.7cm (in microns, don't ask me why they put this is in microns).
+                    height: 210000, // 21cm (in microns)
+                },
+                landscape: true,
+            };
+            return nightmare.viewport(pageDetails.documentArea.width, pageDetails.documentArea.height) // Set the viewport to cover the area of the chart.
+                .pdf(outputImagePath, printOptions) // Capture the entire web page to a PDF report.
+                .end(); // End the Nightmare session. Any queued operations are complated and the headless browser is terminated.
+        });
+};
 webServer.start()
     .then(server => {
-        const nightmare = Nightmare(); // Create an Nightmare instance.
-        return nightmare.goto("http://localhost:3000") // Point the browser at the web server we just started.
-            .wait("svg") // Wait until the graphics for the chart appear on screen. 
-            .evaluate(() => { // Evaluate JavaScript code within the headless browser. This function returns a promise which changes the way our code works.
-                const body = document.querySelector("body"); // Find the body element of the web page.
-                return { // Return details computed in the headless browser to Node.js.
-                    documentArea: { // Return the scrollable area of the page, we will expand the size of the browser window to cover the entire documents (thus removing any scrollbars we might otherwise capture).
-                        width: body.scrollWidth,
-                        height: body.scrollHeight
-                    },
-                };
-            })
-            .then(pageDetails => { // Retrieve details computed in the headless browser. We can now use these value in subsequent Node.js code.
-                const outputImagePath = "./output/nyc-temperature-report.pdf";
-                console.log(">> " + outputImagePath);
-                const printOptions = {
-                    marginsType: 0, // No margins. We want to be able to set our margins explicitly in CSS.
-                    pageSize: { // The size of each page. These values match the specification for the A4 page size standard, but in landscape.
-                        width: 297000, // 29.7cm (in microns, don't ask me why they put this is in microns).
-                        height: 210000, // 21cm (in microns)
-                    },
-                    landscape: true,
-                };
-                return nightmare.viewport(pageDetails.documentArea.width, pageDetails.documentArea.height) // Set the viewport to cover the area of the chart.
-                    .pdf(outputImagePath, printOptions) // Capture the entire web page to a PDF report.
-                    .end(); // End the Nightmare session. Any queued operations are complated and the headless browser is terminated.
-            })
+        const urlToCapture = "http://localhost:3000";
+        const outputImagePath = "./output/nyc-temperatures.png";
+        return captureReport(urlToCapture, "svg", outputImagePath)
             .then(() => server.close()); // Stop the web server when we are done.
     })
     .then(() => {
@@ -40,4 +51,4 @@ webServer.start()
     .catch(err => {
         console.error("Something went wrong :(");
         console.error(err);
-    })
+    });
