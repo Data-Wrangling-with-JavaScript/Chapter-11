152dececc5c80cfd142b102392804df3d7c395f7
Ashley Davis <ashley@codecapers.com.au>
2018-03-15 08:37:15 +1000

    Added support to xvfb to the vagrant box.

--- listing-8/provision.sh
+++ listing-8/provision.sh
@@ -7,3 +7,6 @@ echo "============== Nodejs =================="
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y --force-yes nodejs
+
+echo "============== Support for xvfb =================="
+sudo apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps clang libdbus-1-dev libgtk2.0-dev libnotify-dev libgnome-keyring-dev libgconf2-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev libxss1 libnss3-dev gcc-multilib g++-multilib
